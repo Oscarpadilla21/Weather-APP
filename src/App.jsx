2240b40b0e74e084e10e65d88a77d672135a60e9
Oscar Padilla <davidpadillalastre2107@gmail.com>
2023-05-03 15:59:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Box, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${
-  import.meta.env.VITE_API_KEY
+  "4d3b1308825a4b13978163328230305"
 }&lang=es&q=`;
 
 export default function App() {
